d33a68efcaf4a95eb7c273088e1546fcc19492ab
Zogfryt <Zogfryt@gmail.com>
2025-04-23 20:29:23 +0200

    To the lastest commit

--- app/pages/2_Data_Analysis.py
+++ app/pages/2_Data_Analysis.py
@@ -2,7 +2,7 @@ from streamlit import multiselect, session_state, text_input, button, plotly_cha
 from shared import init
 from loader import Neo4jExecutor
 import plotly.express as px
-from typing import Dict, Tuple
+from typing import Literal
 from pandas import Series, DataFrame
 from clustering import GraphClusterer
 from community_analyser import Analyzer
@@ -11,7 +11,7 @@ from typing import Set
 
 logging.basicConfig(level=logging.INFO)
 
-def entity_plot(data: Dict[Tuple[str,str],int]):
+def entity_plot(data: dict[tuple[str,str],int]):
     entity_dict = dict()
     for key, value in data.items():
         entity_dict[key[0]] = entity_dict.get(key[0],0) + value
@@ -20,7 +20,7 @@ def entity_plot(data: Dict[Tuple[str,str],int]):
     fig = px.bar(dfs.iloc[:20],title='Most frequent entities in the same document as searched entity')
     plotly_chart(fig)
     
-def category_plot(data: Dict[Tuple[str,str],int]):
+def category_plot(data: dict[tuple[str,str],int]):
     category_dict = dict()
     for key, value in data.items():
         category_dict[key[1]] = category_dict.get(key[1],0) + value
@@ -29,18 +29,18 @@ def category_plot(data: Dict[Tuple[str,str],int]):
     fig = px.bar(dfs.iloc[:20],title='Most frequent types of entities in the same document as searched entity')
     plotly_chart(fig)
     
-def combined_plot(data: Dict[Tuple[str,str],int]):
+def combined_plot(data: dict[tuple[str,str],int]):
     dfs = Series({f"{key[0]}({key[1]})": val for key, val in data.items()})
     print(dfs.head())
     dfs.sort_values(inplace=True,ascending=False)
     fig = px.bar(dfs.iloc[:20],title='Most frequent entities with types in the same document as searched entity')
     plotly_chart(fig)
 
-def has_files_changed(selections: Set[str]) -> bool:
-    if len(session_state['analyzed_files']) == 0 and len(selections) > 0:
+def has_files_changed(selections: Set[str], mode: Literal['articles','entities']) -> bool:
+    if len(session_state[f'analyzed_files_{mode}']) == 0 and len(selections) > 0:
         return True
-    union = selections.union(session_state['analyzed_files'])
-    intersection = selections.intersection(session_state['analyzed_files'])
+    union = selections.union(session_state[f'analyzed_files_{mode}'])
+    intersection = selections.intersection(session_state[f'analyzed_files_{mode}'])
     return len(union - intersection) > 0
 
 def show_statistics(n_nodes: int, modularity_score: float):
@@ -48,14 +48,35 @@ def show_statistics(n_nodes: int, modularity_score: float):
     col1.metric("Number of nodes", n_nodes)
     col2.metric("Modularity score", f"{modularity_score: .3f}")
 
+def calculate_and_show_chart(mode: Literal['articles','entities'], files_changed: bool):
+    graph_name = 'DocumentWithDistance' if mode == 'articles' else 'EntitiesWithCoExistance'
+    if select_btn and files_changed:
+        session_state[f'analyzed_files_{mode}'] = set(selections)
+        session_state[f'graph_{mode}'] = cluster.create_graph_projection(selections,graph_name)
+        session_state[f'leiden_result_{mode}'] = cluster.leiden_cluster(session_state[f'graph_{mode}'])
+        cluster.delete_graph_projection(graph_name)
+        loader.update_with_communities(session_state[f'leiden_result_{mode}'][['nodeId','communityId']].to_dict(orient='records'),mode)    
+        session_state[f'modularities_{mode}'] = analyzer.calculate_modularity(selections, mode)
+    df = session_state.get(f'leiden_result_{mode}',DataFrame({'communityId': [], 'nodeId': []}))
+    aggregated_df = df.groupby('communityId').aggregate({'nodeId': list}) 
+    choice = selectbox(label='Choose community ID', options=aggregated_df.index)
+    if choice is not None:
+        df_modularities: DataFrame = session_state[f'modularities_{mode}']
+        show_statistics(len(aggregated_df.loc[choice,'nodeId']),df_modularities.loc[choice, 'modularity'])
+        result = analyzer.get_ents_from_community(aggregated_df.loc[choice,'nodeId'], mode)
+        result = result.sort_values('entityCount',ascending=False)
+        fig = px.bar(result.iloc[:20],title='Top 20 entities')
+        plotly_chart(fig)
 
 init()
 
 loader: Neo4jExecutor = session_state['loader']
+cluster: GraphClusterer = session_state['cluster_driver']
+analyzer: Analyzer = session_state['analyzer']
 selections = multiselect('Ask data from json file',loader.get_files(),[])
-analyzed_files_changed = has_files_changed(set(selections))
+files_changed_articles, files_changed_ents = has_files_changed(set(selections),'articles'), has_files_changed(set(selections),'entities')
 select_btn = button('Select')
-entities, clustering = tabs(['entities', 'clustering'])
+entities, clustering_articles, clustering_ents = tabs(['entities', 'clustering - articles', 'clustering - ents'])
 with entities:
     if select_btn:
         session_state['ents'] = loader.get_all_ners(selections)
@@ -73,25 +94,7 @@ with entities:
         entity_plot(counts)
         category_plot(counts)
         combined_plot(counts)
-with clustering:
-    cluster: GraphClusterer = session_state['cluster_driver']
-    analyzer: Analyzer = session_state['analyzer']
-    if select_btn and analyzed_files_changed:
-        session_state['analyzed_files'] = set(selections)
-        session_state['graph'] = cluster.create_graph_projection(selections)
-        session_state['leiden_result'] = cluster.leiden_cluster(session_state['graph'])
-        cluster.delete_graph_projection()
-        loader.update_with_communities(session_state['leiden_result'][['nodeId','communityId']].to_dict(orient='records'))    
-        session_state['modularities'] = analyzer.calculate_modularity(selections)
-    df = session_state.get('leiden_result',DataFrame({'communityId': [], 'nodeId': []}))
-    aggregated_df = df.groupby('communityId').aggregate({'nodeId': list}) 
-    choice = selectbox(label='Choose community ID', options=aggregated_df.index)
-    community_search_button = button('Search Community')
-    if choice is not None:
-        df_modularities: DataFrame = session_state['modularities']
-        show_statistics(len(aggregated_df.loc[choice,'nodeId']),df_modularities.loc[choice, 'modularity'])
-        result = analyzer.get_ents_from_community(aggregated_df.loc[choice,'nodeId'])
-        result = result.sort_values('entityCount',ascending=False)
-        fig = px.bar(result.iloc[:20],title='Top 20 entities')
-        plotly_chart(fig)
-    
+with clustering_articles:
+    calculate_and_show_chart('articles',files_changed_articles)
+with clustering_ents:
+    calculate_and_show_chart('entities',files_changed_ents)
